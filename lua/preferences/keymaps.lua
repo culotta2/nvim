de5d8b0b99eee6108df662d82814d62a3da77a32
Dominic Culotta <culotta2@illinois.edu>
2024-06-11 20:30:42 -0500

    starting nvim rewrite

---
+++ lua/preferences/keymaps.lua
@@ -0,0 +1,23 @@
+-- Turn off space when used by itself
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Copy & paste with system clipboard
+vim.keymap.set("n", "<leader>cy", "\"+yy", { desc = "[Y]ank current line"})
+vim.keymap.set("v", "<leader>cy", "\"+y", { desc = "[Y]ank current visual selection"})
+vim.keymap.set("n", "<leader>cp", "\"+p", { desc = "[P]aste into current line"})
+
+-- Move page up and down but keep cursor centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz_", { desc = "[D]own a page (1/2 a screen)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz_", { desc = "[U]p a page (1/2 a screen)" })
+
+-- Center search words
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Clear search highlight
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+
+-- Diagnostics
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous [D]iagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next [D]iagnostic" })
+
---
+++ lua/preferences/settings.lua
@@ -0,0 +1,53 @@
+-- General
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = true
+vim.opt.cursorline = true
+vim.opt.inccommand = 'split'
+vim.opt.showmode = false
+vim.opt.signcolumn = 'yes'
+vim.opt.undofile = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Display characters
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- Casing
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Highlight search
+vim.opt.hlsearch = true
+
+-- New split behavior
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Coloring
+-- vim.opt.termguicolors = true
+-- vim.cmd("set background=dark")
+
+-- Scroll
+vim.opt.scrolloff = 10
+
+-- Use ripgrep
+vim.opt.grepprg = "rg --hidden --vimgrep --smart-case --"
+
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = "menuone,noselect"
+
+-- Disable the mouse
+vim.opt.mouse = nil
+
+-- Set shell
+vim.opt.shell = "zsh"
+
