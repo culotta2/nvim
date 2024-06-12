4813ba466032bd86c381fbd6ef4fde2b4fd701bc
Dominic Culotta <culotta2@illinois.edu>
2024-06-12 09:04:41 -0500

    Fixed todo comments and autocomplete

--- lua/plugins/nvim-lsp-config.lua
+++ lua/plugins/nvim-lsp-config.lua
@@ -13,7 +13,12 @@ return {
 
 		-- Additional lua configuration
 		{ "folke/neodev.nvim", opts = {} },
-		{ "folke/neodev.nvim", opts = {} },
+
+		-- Autocomplete
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		-- "L3MON4D3/LuaSnip",
+		-- "saadparwaiz1/cmp_luasnip",
 	},
 	config = function()
 		local lsp_group = vim.api.nvim_create_augroup("lsp_group", { clear = true })
@@ -48,57 +53,24 @@ return {
 					end,
 					{ desc = "LSP: [Toggle] inlay [H]ints" }
 				)
-				-- vim.keymap.set(
-				-- 	"n", "<leader>th",
-				-- 	function()
-				-- 	-- 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-				-- 		print("Hello")
-				-- 	end
-				-- 	{ desc = "LSP: Toggle inlay [H]ints" },
-				-- )
-				-- Original
 			end,
 		})
 
-		-- vim.api.nvim_create_autocmd("LspAttach", {
-		-- 	group = lsp_group,
-		-- 	callback = function(event)
-		-- 		vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "LSP: [G]oto [D]efinition" })
-		-- 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration" })
-		-- 		vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP: [G]oto [R]eferences" })
-		-- 		vim.keymap.set("n", "gI", builtin.lsp_implementation, { desc = "LSP: [G]oto [I]mplementation" })
-		-- 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
-		-- 		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "LSP: Global [R]ename" })
-		-- 		vim.keymap.set("n", "<leader>lca", vim.lsp.buf.code_action, { desc = "LSP: Code [A]ction" })
-		-- 		vim.keymap.set("n", "<leader>lt", builtin.lsp_type_definitions, { desc = "LSP: [T]ype definition" })
-		-- 		vim.keymap.set("n", "<leader>lds", builtin.lsp_document_symbols,
-		-- 			{ desc = "LSP: [D]ocument [S]ymbols" }
-		-- 		)
-		-- 		vim.keymap.set("n", "<leader>lws", builtin.lsp_dynamic_workspace_symbols, 
-		-- 			{ desc = "LSP: [D]ocument [S]ymbols" }
-		-- 		)
-		-- 		vim.keymap.set("n", "<leader>lh", vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()),
-		-- 			{ desc = "LSP: Toggle inlay [H]ints" }
-		-- 		)
-		-- 	end
-		-- })
-
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		-- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 		local servers = {
 			based_pyright = {},
-			rust_analyzer = {}
-		}
-
-		local lua_ls = {
-			settings = {
-				Lua = {
-					completion = {
-						callsnippet = "Replace",
-					},
-					diagnostics = { disable = { "missing-fields" }},
+			rust_analyzer = {},
+			lua_ls = {
+				settings = {
+					Lua = {
+						completion = {
+							callsnippet = "Replace",
+						},
+						diagnostics = { disable = { "missing-fields" }},
+					}
 				}
-			}
+			},
 		}
 		require("mason").setup()
 		require('mason-lspconfig').setup {
---
+++ lua/plugins/todo-comments-config.lua
@@ -0,0 +1,20 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+
+	opts = {
+		keywords = {
+			TODO = { alt = { "todo", "unimplemented" } },
+		},
+		highlight = {
+			pattern = {
+				[[.*<(KEYWORDS)\s*:]],
+				[[.*<(KEYWORDS)\s*!\(]],
+			},
+			comments_only = false,
+		},
+		search = {
+			pattern = [[\b(KEYWORDS)(:|!\()]],
+		},
+	},
+}
