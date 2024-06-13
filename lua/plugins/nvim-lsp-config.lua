23014cf5cfc20fd75ff6e4850de96c9e079fe9d9
Dominic Culotta <culotta2@illinois.edu>
2024-06-12 20:01:34 -0500

    Fixed python LSP

--- lua/plugins/nvim-lsp-config.lua
+++ lua/plugins/nvim-lsp-config.lua
@@ -59,7 +59,7 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 		local servers = {
-			based_pyright = {},
+			basedpyright = {},
 			rust_analyzer = {},
 			lua_ls = {
 				settings = {
