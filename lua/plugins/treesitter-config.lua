0954803d2e543b34c26dca5130921e7c09131f8b
Dominic Culotta <culotta2@illinois.edu>
2024-09-28 01:00:55 -0500

    Finally figured out treesitter textobjects

--- lua/plugins/treesitter-config.lua
+++ lua/plugins/treesitter-config.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects"
+	},
 	build = ':TSUpdate',
+	main = "nvim-treesitter.configs",
 	opts = {
 		ensure_installed = {
 			"bash",
@@ -11,30 +15,67 @@ return {
 			"luadoc",
 			"markdown",
 			"python",
+			"r",
 			"rust",
 			"vim",
 			"vimdoc",
 		},
 		-- Autoinstall languages that are not installed
 		auto_install = true,
+		sync_install = true,
 		highlight = {
 			enable = true,
-			additional_vim_regex_highlighting = { "python" },
+			-- additional_vim_regex_highlighting = { "python" },
 		},
-		indent = { enable = true, disable = { "python" } },
+		-- indent = { enable = true, disable = { "python" } },
+		indent = { enable = true },
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+					["ii"] = "@conditional.inner",
+					["ai"] = "@conditional.outer",
+					["il"] = "@loop.inner",
+					["al"] = "@loop.outer",
+					["at"] = "@comment.outer",
+				}
+			},
+			move = {
+				enable = true,
+				set_jumps = true,
+				goto_next_start = {
+					["]f"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]F"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[f"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[F"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>pa"] = "@parameter.inner",
+				},
+				swap_previous = {
+					["<leader>pA"] = "@parameter.inner",
+				},
+			},
+		}
 	},
-	config = function(_, opts)
-		-- Prefer git instead of curl in order to improve connectivity in some environments
-		require('nvim-treesitter.install').prefer_git = true
-		---@diagnostic disable-next-line: missing-fields
-		require('nvim-treesitter.configs').setup(opts)
-
-		-- TODO: Look into textobjects
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-	end,
 }
