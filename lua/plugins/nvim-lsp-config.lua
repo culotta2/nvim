a087da1176da4fe4b6abb754b7d5b351ad79ddd0
Dominic Culotta <dculotta4321@gmail.com>
2025-02-16 22:45:03 -0600

    Replaced telescope with snacks picker

--- lua/plugins/nvim-lsp-config.lua
+++ lua/plugins/nvim-lsp-config.lua
@@ -20,7 +20,7 @@ return {
 	},
 	config = function()
 		local lsp_group = vim.api.nvim_create_augroup("lsp_group", { clear = true })
-		local builtin = require("telescope.builtin")
+		-- local builtin = require("telescope.builtin")
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Function to run when the LSP is attached
@@ -28,18 +28,18 @@ return {
 			group = lsp_group,
 			callback = function(args)
 				-- LSP Keymaps
-				vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "LSP: [G]oto [D]efinition" })
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration" })
-				vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP: [G]oto [R]eferences" })
-				vim.keymap.set("n", "gI", builtin.lsp_implementations, { desc = "LSP: [G]oto [I]mplementation" })
+				vim.keymap.set("n", "gd", Snacks.picker.lsp_definitions, { desc = "LSP: [G]oto [D]efinition" })
+				vim.keymap.set("n", "gD", Snacks.picker.lsp_declarations, { desc = "LSP: [G]oto [D]eclaration" })
+				vim.keymap.set("n", "gr", Snacks.picker.lsp_references, { desc = "LSP: [G]oto [R]eferences" })
+				vim.keymap.set("n", "gI", Snacks.picker.lsp_implementations, { desc = "LSP: [G]oto [I]mplementation" })
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Global [R]ename" })
 				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code [A]ction" })
-				vim.keymap.set("n", "<leader>D", builtin.lsp_type_definitions, { desc = "LSP: [T]ype definition" })
-				vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols,
+				vim.keymap.set("n", "<leader>sy", Snacks.picker.lsp_type_definitions, { desc = "LSP: [G]oto T[y]pe Definitions" })
+				vim.keymap.set("n", "<leader>ds", Snacks.picker.lsp_symbols,
 					{ desc = "LSP: [D]ocument [S]ymbols" }
 				)
-				vim.keymap.set("n", "<leader>ws", builtin.lsp_dynamic_workspace_symbols,
+				vim.keymap.set("n", "<leader>ws", Snacks.picker.lsp_workspace_symbols,
 					{ desc = "LSP: [D]ocument [S]ymbols" }
 				)
 				vim.keymap.set(
