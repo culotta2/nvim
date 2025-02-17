3bdc3438bf143b852569ecbe0fe07913fc4ab24e
Dominic Culotta <dculotta4321@gmail.com>
2025-02-16 21:33:14 -0600

    Added image support

--- lua/plugins/snacks-config.lua
+++ lua/plugins/snacks-config.lua
@@ -6,6 +6,7 @@ return {
 		bigfile = { enabled = true },
 		dashboard = { enabled = true },
 		dim = { enabled = true },
+		image = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
 		notifier = { enabled = true },
