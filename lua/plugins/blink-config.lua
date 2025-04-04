74486f9fccb65cbccaa1826895af2dd9843b8398
Dominic Culotta <dculotta4321@gmail.com>
2025-04-03 21:56:08 -0500

    Added custom keys for snippet navigation

--- lua/plugins/blink-config.lua
+++ lua/plugins/blink-config.lua
@@ -6,7 +6,11 @@ return {
 	---@module "blink.cmp"
 	---@type blink.cmp.Config
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "default",
+			['<C-l>'] = {'snippet_forward', 'fallback'},
+			['<C-h>'] = {'snippet_backward', 'fallback'},
+		},
 		appearance = {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono"
