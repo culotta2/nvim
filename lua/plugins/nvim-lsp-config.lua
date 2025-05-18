3cb13eb046456ee4c2809b4737b75194579f2bda
Dominic Culotta <dculotta4321@gmail.com>
2025-05-18 15:04:54 -0500

    fix(lsp): Prevent rust analyzer from starting twice

--- lua/plugins/nvim-lsp-config.lua
+++ lua/plugins/nvim-lsp-config.lua
@@ -20,7 +20,6 @@ return {
 	},
 	config = function()
 		local lsp_group = vim.api.nvim_create_augroup("lsp_group", { clear = true })
-		-- local builtin = require("telescope.builtin")
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Function to run when the LSP is attached
@@ -28,14 +27,20 @@ return {
 			group = lsp_group,
 			callback = function(args)
 				-- LSP Keymaps
-				vim.keymap.set("n", "gd", Snacks.picker.lsp_definitions, { desc = "LSP: [G]oto [D]efinition" })
-				vim.keymap.set("n", "gD", Snacks.picker.lsp_declarations, { desc = "LSP: [G]oto [D]eclaration" })
-				vim.keymap.set("n", "gr", Snacks.picker.lsp_references, { desc = "LSP: [G]oto [R]eferences" })
-				vim.keymap.set("n", "gI", Snacks.picker.lsp_implementations, { desc = "LSP: [G]oto [I]mplementation" })
+				vim.keymap.set("n", "gd", Snacks.picker.lsp_definitions,
+					{ desc = "LSP: [G]oto [D]efinition" })
+				vim.keymap.set("n", "gD", Snacks.picker.lsp_declarations,
+					{ desc = "LSP: [G]oto [D]eclaration" })
+				vim.keymap.set("n", "gr", Snacks.picker.lsp_references,
+					{ desc = "LSP: [G]oto [R]eferences" })
+				vim.keymap.set("n", "gI", Snacks.picker.lsp_implementations,
+					{ desc = "LSP: [G]oto [I]mplementation" })
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: Global [R]ename" })
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code [A]ction" })
-				vim.keymap.set("n", "<leader>sy", Snacks.picker.lsp_type_definitions, { desc = "LSP: [G]oto T[y]pe Definitions" })
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,
+					{ desc = "LSP: Code [A]ction" })
+				vim.keymap.set("n", "<leader>sy", Snacks.picker.lsp_type_definitions,
+					{ desc = "LSP: [G]oto T[y]pe Definitions" })
 				vim.keymap.set("n", "<leader>ds", Snacks.picker.lsp_symbols,
 					{ desc = "LSP: [D]ocument [S]ymbols" }
 				)
@@ -57,45 +62,35 @@ return {
 
 		-- Set up LSPs
 		require("mason").setup()
-		require("mason-lspconfig").setup({})
 
-		-- Lua
-		require("lspconfig").lua_ls.setup({
+		vim.lsp.enable({ "basedpyright", "marksman", "sqls", "tinymist", "lua_ls" })
+
+		vim.lsp.config("basedpyright", {
+			capabilities = capabilities,
+		})
+		vim.lsp.config("sqls", {
+			capabilities = capabilities,
+		})
+		vim.lsp.config("marksman", {
+			capabilities = capabilities,
+		})
+		vim.lsp.config("tinymist", {
+			capabilities = capabilities,
+			settings = {
+				exportPdf = "onSave",
+			}
+		})
+		vim.lsp.config("lua_ls", {
 			capabilities = capabilities,
 			settings = {
 				Lua = {
 					completion = {
 						callsnippet = "Replace",
 					},
-					diagnostics = { disable = { "missing-fields" }},
+					diagnostics = {
+						disable = { "missing-fields" },
+					}
 				}
-			}
-		})
-		-- Python
-		require("lspconfig").basedpyright.setup({
-			capabilities = capabilities,
-		})
-
-		-- Rust analyzer
-		require("mason-lspconfig").setup_handlers {
-			["rust_analyzer"] = function() end,
-		}
-
-		-- SQL
-		require("lspconfig").sqls.setup({
-			capabilities = capabilities,
-		})
-
-		-- Markdown
-		require("lspconfig").marksman.setup({
-			capabilities = capabilities,
-		})
-
-		-- Typst
-		require("lspconfig").tinymist.setup({
-			capabilities = capabilities,
-			settings = {
-				exportPdf = "onSave", -- onType, never
 			}
 		})
 	end,
